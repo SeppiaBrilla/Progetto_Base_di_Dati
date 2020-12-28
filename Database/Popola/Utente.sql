1c89148be6080ae804efd78970eab54c019b1fe5
WilliamArnone <arnonewilliam@gmail.com>
2020-12-28 21:22:32 +0100

    finito db

---
+++ Database/Popola/Utente.sql
@@ -0,0 +1,10 @@
+INSERT INTO `Videogiochi_e_software`.`Utente` (`Nickname`) VALUES ('Gabibbo');
+INSERT INTO `Videogiochi_e_software`.`Utente` (`Nickname`) VALUES ('Malgioglio');
+INSERT INTO `Videogiochi_e_software`.`Utente` (`Nickname`) VALUES ('Marco Aurelio');
+INSERT INTO `Videogiochi_e_software`.`Utente` (`Nickname`) VALUES ('Pippo');
+INSERT INTO `Videogiochi_e_software`.`Utente` (`Nickname`) VALUES ('Platone');
+INSERT INTO `Videogiochi_e_software`.`Utente` (`Nickname`) VALUES ('SeppiaBrilla');
+INSERT INTO `Videogiochi_e_software`.`Utente` (`Nickname`) VALUES ('WilliaMan');
+INSERT INTO `Videogiochi_e_software`.`Utente` (`Nickname`) VALUES ('Xx_DarkAngelCraft_xX');
+INSERT INTO `Videogiochi_e_software`.`Utente` (`Nickname`) VALUES ('Zeb89');
+
