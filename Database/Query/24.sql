da3d5ab4366bb904ffcc2164b3b2d14f8ccef67d
WilliamArnone <arnonewilliam@gmail.com>
2020-12-29 16:42:58 +0100

    modifiche

--- Database/Query/24.sql
+++ Database/Query/24.sql
@@ -1,1 +1,5 @@
-SELECT * FROM Videogiochi_e_software.Saga;
+SELECT S.Nome AS Nome, S.Descrizione AS Descrizione, S.Stato AS Stato, MIN(P.Anno) AS Nascita
+FROM Videogiochi_e_software.Saga as S, Videogiochi_e_software.Prodotto AS P
+WHERE P.Saga = S.Nome
+GROUP BY S.Nome
+ORDER BY MIN(P.Anno);
