991c959d9cd01ef539f7ff7a75b70ca674597248
WilliamArnone <arnonewilliam@gmail.com>
2021-01-02 14:11:25 +0100

    38 fix

--- Database/Query/38.sql
+++ Database/Query/38.sql
@@ -1,3 +1,3 @@
-SELECT DISTINCT Prod.Sviluppatore AS Sviluppatori
+SELECT DISTINCT Prod.Produttore AS Produttori
 FROM Videogiochi_e_software.Compagnia AS Comp INNER JOIN Videogiochi_e_software.Prodotto AS Prod ON Comp.Nome = Prod.Produttore
-WHERE Comp.Nome = 'Microsoft' AND Prod.Gioco IS TRUE;
+WHERE Prod.Sviluppatore = 'Microsoft' AND Prod.Gioco IS TRUE;
